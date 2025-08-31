ae1d5c51dab6bc468ac9e396062c4a7680a2ab08
Werwarmort <werwarmort256@gmail.com>
2025-08-31 20:12:43 +0300

    feat(package.json): добавил моды dev и prod при сборке

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -7,6 +7,9 @@ import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
   const { mode, paths } = options;
+
+  const isDev = mode === "development";
+
   return {
     mode: mode,
     entry: paths.entry,
@@ -20,7 +23,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
     },
     resolve: buildResolvers(),
     plugins: buildPlugins(options),
-    devtool: 'inline-source-map',
-    devServer: buildDevServer(options),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   }
 }
