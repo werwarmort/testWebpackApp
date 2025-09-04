faae7339b3a2256c5c4daf30ca882909fdfbfa74
Werwarmort <werwarmort256@gmail.com>
2025-09-04 12:27:38 +0300

    feat(App.tsx): добавлено historyApiFallback для проксирования запроса на документ, чтобы не получать 404 ошибку

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,5 +6,6 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
   return {
     port: options.port,
     open: true,
+    historyApiFallback: true, // проксирование запросов через корневую страницу, чтобы не было ошибки 404 на документ
   }
 }
