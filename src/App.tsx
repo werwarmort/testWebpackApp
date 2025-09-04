e3a6b67a9a3f1b82dd8b0fb4cfb03556f141bdfd
Werwarmort <werwarmort256@gmail.com>
2025-09-04 12:48:18 +0300

    feat: разбиение кода на чанки

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import Counter from "./components/Conunter";
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom'
 import './index.scss'
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
   return (
     <div className='app'>
       <Link to='/'>Главная</Link>
       <Link to='/about'>О сайте</Link>
+      <Suspense fallback={<div>loading</div>}>
       <Routes>
-        <Route path={'about'} element={<AboutPage />} />
-        <Route path={'/'} element={<MainPage />} />
+
+          <Route path={'about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+
       </Routes>
+
+      </Suspense>
     </div>
   );
 };
