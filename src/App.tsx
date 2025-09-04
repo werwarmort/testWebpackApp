1d3bbd9c05656475123759c4c4dbf2b279729109
Werwarmort <werwarmort256@gmail.com>
2025-09-04 12:25:25 +0300

    feat(App.tsx): добавлены примеры страниц

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Counter from "./components/Conunter";
+import {Link, Route, Routes} from 'react-router-dom'
 import './index.scss'
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 const App = () => {
   return (
     <div className='app'>
-      <Counter />
+      <Link to='/'>Главная</Link>
+      <Link to='/about'>О сайте</Link>
+      <Routes>
+        <Route path={'about'} element={<AboutPage />} />
+        <Route path={'/'} element={<MainPage />} />
+      </Routes>
     </div>
   );
 };
