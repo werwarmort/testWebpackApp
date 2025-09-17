a36f01e541ef369276a95a3f511236587684c067
Werwarmort <werwarmort256@gmail.com>
2025-09-17 10:08:37 +0300

    feat: добавлен и конфигурирован react-refresh-webpack-plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,21 +2,30 @@ import HTMLWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+
 
 export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
 
-  return [
+  const plugins = [
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
       filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "css/[name].[contenthash:8].css", // для разбиения файла на чанки, которые потом будут асинхронно подгружаться
+      chunkFilename: "css/[name].[contenthash:8].css",
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
-  ]
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+
+  return plugins;
 }
 
