ee0f9f93341ea9d0ce4befe7eb81c2f2370d53e6
Werwarmort <werwarmort256@gmail.com>
2025-12-02 18:37:29 +0300

    feat(NotFoundPage): интегрировал 404 страницу в роуты, добавил стили

---
+++ src/2_Pages/NotFoundPage/index.ts
@@ -0,0 +1,2 @@
+import { NotFoundPage } from "2_Pages/NotFoundPage/ui/NotFoundPage";
+export { NotFoundPage };
--- src/6_Shared/config/routerConfig/routerConfig.tsx
+++ src/6_Shared/config/routerConfig/routerConfig.tsx
@@ -1,6 +1,7 @@
 import { MainPage } from '2_Pages/MainPage';
 import { AboutPage } from '2_Pages/AboutPage';
 import { RouteProps } from 'react-router-dom';
+import {NotFoundPage} from "2_Pages/NotFoundPage";
 
 // interface AppRouteProps {
 //   path: string;
@@ -10,11 +11,13 @@ import { RouteProps } from 'react-router-dom';
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
+  NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -25,5 +28,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.ABOUT]: {
         path: RoutePath.about,
         element: <AboutPage />,
+    },
+    [AppRoutes.NOT_FOUND]: {
+      path: RoutePath.not_found,
+      element: <NotFoundPage />,
     },
 };
