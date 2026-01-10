903d8e79fe85fe60e67d0b46279e7485394d9252
Werwarmort <werwarmort256@gmail.com>
2026-01-10 15:51:01 +0300

    fix: правка типов

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -9,6 +9,8 @@ export default ({ config }: { config: webpack.Configuration }) => {
         html: '',
         entry: '',
         src: path.resolve(process.cwd(), 'src'),
+        locales: '',
+        buildLocales: '',
     };
 
     config.resolve.modules.push(paths.src);
